15c3dbd823a81a9bd9217aacc99d941e7d152d9c
Mayor <akintayomayomiolamide@gmail.com>
2022-11-29 20:51:49 -0800

    if

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(10):
+    for b in range(i + 1, 10):
+        if i == 8 and b == 9:
+            print("{}{}".format(i, b))
+        else:
+            print("{}{}".format(i, b), end=", ")
