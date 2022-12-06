e0e23e1a41ad4636bf8634b06594b4bc9b62be17
Mayor <akintayomayomiolamide@gmail.com>
2022-12-05 18:30:46 -0800

    python

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element)
-    if idx >= 0 and idx < len(my_list):
-        my_list.insert(idx, element)
-    return my_list
+def replace_in_list(my_list, idx, element):
+
+    if idx < 0:
+        return (my_list)
+    elements = len(my_list)
+
+    if idx > elements - 1:
+        return (my_list)
+
+    my_list[idx] = element
+    return (my_list)
